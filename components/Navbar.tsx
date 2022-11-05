57b03359fd3069268cba7f0dfce5d4e4c0fa809a
Toromo A <toromo.ade06@gmail.com>
2022-11-05 05:08:17 -0500

    tit tok backend and utils

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import {AiOutlineLogout} from 'react-icons/ai'
+import {BiSearch} from 'react-icons/bi'
+import {IoMdAdd} from 'react-icons/io'
+import {FaHome} from 'react-icons/fa'
 
 interface Props { 
-    
+
 }
 
 const Navbar: NextComponentType<NextPageContext, {}, Props> = (props: Props,) => {
@@ -13,7 +17,9 @@ const Navbar: NextComponentType<NextPageContext, {}, Props> = (props: Props,) =>
         <div>
             <div className="flex items-center justify-between px-4 py-2 bg-white shadow-lg">
                 <div className="flex items-center gap-4">
-                    <div className="w-10 h-10">
+                    <div className="">
+                        <Image src="/tik-tok-logo.svg" alt='logo' width={100} height={50}/>
+
                     </div>
                 </div>
             </div>
