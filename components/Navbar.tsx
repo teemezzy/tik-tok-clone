2d6531f0aa2d0301b3bc424e1356683b3f26fd88
Toromo A <toromo.ade06@gmail.com>
2022-11-05 05:37:40 -0500

    api token response

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,12 +15,14 @@ interface Props {
 const Navbar: NextComponentType<NextPageContext, {}, Props> = (props: Props,) => {
     return (
         <div>
-            <div className="flex items-center justify-between px-4 py-2 bg-white shadow-lg">
+            <div className=" w-full flex items-center justify-between px-4 py-2 bg-white shadow-lg">
                 <div className="flex items-center gap-4">
-                    <div className="">
-                        <Image src="/tik-tok-logo.svg" alt='logo' width={100} height={50}/>
+                    <Link href='/'>
+                    <div className="w-[100px] md:w-[130px]">
+                        <Image src="/tik-tok-logo.svg" alt='logo' width={100} height={50} className='cursor-pointer'/>
 
                     </div>
+                    </Link>
                 </div>
             </div>
         </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,24 @@
 import type { NextComponentType, NextPageContext } from "next";
+import {NextPage} from 'next'
+import {useState} from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import {AiFillHome, AiOutlineMenu} from 'react-icons/ai'
+import{ImCancelCircle} from 'react-icons/im'
 
 interface Props {}
 
 const Sidebar: NextComponentType<NextPageContext, {}, Props> = (
   props: Props,
 ) => {
+  const [showsidebar, setShowsidebar] = useState(true)
   return (
     <div>
+      <div className='block xl:hidden m-2 ml-4 mt-3 bg-white text-xl shadow-2xl'>
+      
+        
+      </div>
         
     </div>
   )
