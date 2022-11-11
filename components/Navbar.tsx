f4dd4cf53f7dced1ab3d5932dc80925e03fcb434
Toromo A <toromo.ade06@gmail.com>
2022-11-11 15:06:48 -0600

    sidebar and navbar process and cleanups

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,25 +3,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
-import {AiOutlineLogout} from 'react-icons/ai'
-import {BiSearch} from 'react-icons/bi'
-import {IoMdAdd} from 'react-icons/io'
-import {FaHome} from 'react-icons/fa'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { BiSearch } from 'react-icons/bi'
+import { IoMdAdd } from 'react-icons/io'
+import { FaHome } from 'react-icons/fa'
 
-interface Props { 
+interface Props {
 
 }
 
 const Navbar: NextComponentType<NextPageContext, {}, Props> = (props: Props,) => {
     return (
         <div>
-            <div className=" w-full flex items-center justify-between px-4 py-2 bg-white shadow-lg">
-                <div className="flex items-center gap-4">
+            <div className=" w-full flex items-center justify-between px-4 py-7 bg-white shadow-lg">
+                <div className="flex items-center gap-4 ">
                     <Link href='/'>
-                    <div className="w-[100px] md:w-[130px]">
-                        <Image src="/tik-tok-logo.svg" alt='logo' width={100} height={50} className='cursor-pointer'/>
+                        <div className="w-[100px] md:w-[130px]">
 
-                    </div>
+                            <Image src="/tik-tok-logo.svg" alt='logo' width={100} height={50} priority className='cursor-pointer w-auto h-auto' />
+
+                        </div>
                     </Link>
                 </div>
             </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,27 +1,34 @@
 import type { NextComponentType, NextPageContext } from "next";
-import {NextPage} from 'next'
-import {useState} from 'react'
+import { NextPage } from 'next'
+import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
-import {AiFillHome, AiOutlineMenu} from 'react-icons/ai'
-import{ImCancelCircle} from 'react-icons/im'
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
+import { ImCancelCircle } from 'react-icons/im'
 
-interface Props {}
+interface Props { }
 
 const Sidebar: NextComponentType<NextPageContext, {}, Props> = (
   props: Props,
 ) => {
-  const [showsidebar, setShowsidebar] = useState(true)
+  const [showSidebar, setShowSidebar] = useState(true)
+  const router = useRouter()
+
+  const handleClick = () => { showSidebar ? setShowSidebar(false) : setShowSidebar(true) }
+  // const handleClick = () => { setShowSidebar((prev) => !prev) }
+
   return (
     <div>
-      <div className='block xl:hidden m-2 ml-4 mt-3 bg-white text-xl shadow-2xl'>
-      
-        
+      <div className='block xl:hidden m-2 ml-4 mt-3 bg-white text-xl shadow-2xl' onClick={handleClick}>
+        {
+          showSidebar ? <AiOutlineMenu /> : <ImCancelCircle />
+        }
+
       </div>
-        
     </div>
   )
 }
 
 export default Sidebar
+
