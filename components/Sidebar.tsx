3ed7b4ccb390b89a90048e0da5baecf482d1f817
Toromo A <toromo.ade06@gmail.com>
2022-11-16 09:57:07 -0600

    new

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,29 +12,40 @@ interface Props { }
 const Sidebar: NextComponentType<NextPageContext, {}, Props> = (
   props: Props,
 ) => {
-  const [showSidebar, setShowSidebar] = useState(true)
+  const [showSidebar, setShowSidebar] = useState(false)
   const router = useRouter()
 
   const handleClick = () => { showSidebar ? setShowSidebar(false) : setShowSidebar(true) }
   // const handleClick = () => { setShowSidebar((prev) => !prev) }
+  const normalLink = 'flex items-center justify-center xl:justify-start cursor-pointer font-semibold text-gray-800 gap-3 px-4 py-2 rounded-md hover:bg-gray-100';
 
   return (
     <div>
       <div className='block xl:hidden m-2 ml-4 mt-3 bg-white text-xl shadow-2xl' onClick={handleClick}>
         {
-          showSidebar ? <AiOutlineMenu /> : <ImCancelCircle />
+          showSidebar ? <ImCancelCircle /> : <AiOutlineMenu /> 
         }
       </div>
       {showSidebar && (
-        <div className='w-64 h-screen fixed top-0 left-0 bg-white shadow-2xl'>
-          <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
 
-
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
+          <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+              <Link href='/'>
+                <div className={normalLink}>
+                  <p className='text-2xl'>
+                    <AiFillHome className='text-2xl' />
+                  </p>
+                  <span className='text-xl hidden xl:block'>For You</span>
+                </div>
+              </Link>  
           </div>
         </div>
       )}
-      <div className={`fixed top-0 left-0 w-64 h-full bg-white shadow-2xl transform transition-all duration-300 ease-in-out ${showSidebar ? 'translate-x-0' : '-translate-x-full'}`}>
-      </div>
+      {/* <div className={`fixed top-0 left-0 w-64 h-full bg-white shadow-2xl transform transition-all duration-300 ease-in-out ${showSidebar ? 'translate-x-0' : '-translate-x-full'}`}>
+            <div className=' sticky top-0 left-0 shadow-2xl'>
+
+            </div>
+      </div> */}
     </div>
   )
 }
