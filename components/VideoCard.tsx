b17f34a977637dab8c756b410e06afc6333a5873
Toromo A <toromo.ade06@gmail.com>
2022-12-19 16:55:05 -0600

    api

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Link from 'next/link'
+import Image from 'next/image'
 import { Video } from '../types'
 
 interface Props {
@@ -10,7 +12,17 @@ const VideoCard: NextPage<Props> = ({ post }) => {
     return (
         <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
             <div>
-        
+                <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+                    <div className='md:w-16 md:h-16 w-10 h-10'>
+                        <Link href='/'>
+                            <>
+                            <Image src='' alt='' width={62} height={62}/>
+                            </>
+
+                        </Link>
+                    </div>
+                </div>
+
             </div>
             {/* {posts.map((post: Video) => (
                 <div>
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,13 +1,13 @@
 import { allPostsQuery } from './../../../utils/queries';
 import { client } from './../../../utils/client';
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function  handler( req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function  handler({ req, res }: { req: NextApiRequest; res: NextApiResponse<Data> }): Promise<void> {
     if (req.method === 'GET') {
         // Process a GET request 
         const query = allPostsQuery()  
