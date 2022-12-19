e5ca40bbd48d8ad3a5aef1d509fa7789dd6fa2b4
Toromo A <toromo.ade06@gmail.com>
2022-12-19 14:55:52 -0600

    video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,15 +1,26 @@
-import type { NextPage} from "next";
-import {Video} from '../types'
+import type { NextPage } from "next";
+import { Video } from '../types'
 
 interface Props {
     post: Video
 }
 
-const VideoCard: NextPage<Props> = ({post}) => {
-    console.log(post.caption)
-  return (
-    <div></div>
-  )
+const VideoCard: NextPage<Props> = ({ post }) => {
+    // console.log(post.caption)
+    return (
+        <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+            <div>
+        
+            </div>
+            {/* {posts.map((post: Video) => (
+                <div>
+                    <h1>{post.caption}</h1>
+                    <video src={post.videoUrl} controls loop></video>
+                </div>
+            )} */}
+
+        </div>
+    )
 }
 
 export default VideoCard
